accb8df53c19878639ad752248d7bc4f67999077
r4dir4 <rauldira2004@gmail.com>
2026-01-28 09:31:46 +0100

    cambios HiloServidor

--- src/es/studium/ControlTurnos/HiloServidor.java
+++ src/es/studium/ControlTurnos/HiloServidor.java
@@ -1,36 +1,59 @@
 package es.studium.ControlTurnos;
 
-/**
- * @author Raúl
- */
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException; 
 import java.net.Socket;
+import java.util.ArrayList;
 
+/**
+ * @author Raúl
+ */
 public class HiloServidor extends Thread
 { 
 	Socket socket; 
 	DataOutputStream salida; 
 	DataInputStream entrada;  
-	public HiloServidor(Socket s) throws IOException 
+	ArrayList<Ticket> listaTickets;
+
+	public HiloServidor(Socket s, ArrayList<Ticket> lista) throws IOException 
 	{   
-		this.socket = s;   
+		this.socket = s;
+		this.listaTickets = lista;
 		salida = new DataOutputStream(s.getOutputStream()); 
 		entrada = new DataInputStream(s.getInputStream()); 
 	} 
+
 	public void run() 
 	{   
-		String cadena = ""; 
 		try   
 		{   
-			while(!cadena.trim().equals("*"))  
-			{    
-				System.out.println("Comunico con socket " + socket.toString());  
-				cadena = entrada.readUTF();
-				salida.writeUTF(cadena.toUpperCase());
-			}    
-			System.out.println("Fin de comunicación con socket " + socket.toString());    
+			String mensajeRecibido = entrada.readUTF();
+			
+			if (mensajeRecibido.equals("SOLICITAR_TICKET")) 
+			{
+				synchronized (listaTickets) {
+					String numero = "T" + (listaTickets.size() + 1);
+					Ticket nuevoTicket = new Ticket(numero, "28/01/2026", "10:30");
+					listaTickets.add(nuevoTicket);
+					salida.writeUTF(numero);
+				}
+			}
+			else if (mensajeRecibido.equals("SIGUIENTE_PUESTO")) 
+			{
+				synchronized (listaTickets) {
+					if (!listaTickets.isEmpty()) {
+						Ticket ticketAtendido = listaTickets.remove(0);
+						salida.writeUTF(ticketAtendido.getNumero());
+					} else {
+						salida.writeUTF("COLA_VACIA");
+					}
+				}
+			}
+			
+			System.out.println("Operación finalizada con socket " + socket.toString());    
+
+
 			salida.close(); 
 			entrada.close();  
 			socket.close();  
@@ -39,6 +62,5 @@ public class HiloServidor extends Thread
 		{ 
 			e.printStackTrace(); 
 		}
-
 	}
 }

e4cd59c11361c3819ca0650b88ca562541efa011
Atrozz7 <pnesan006@gmail.com>
2026-01-28 09:05:43 +0100

    cliente cambiado

--- src/es/studium/ControlTurnos/Cliente.java
+++ src/es/studium/ControlTurnos/Cliente.java
@@ -1,5 +1,30 @@
 package es.studium.ControlTurnos;
 
-public class Cliente {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 
+public class Cliente extends JFrame {
+	public Cliente() {
+		setTitle("Cliente");
+		getContentPane().setLayout(null);
+		
+		JButton btnTicket = new JButton("Pedir cita");
+		btnTicket.setBounds(103, 11, 224, 42);
+		getContentPane().add(btnTicket);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 58, 414, 181);
+		getContentPane().add(scrollPane);
+		
+		JList<String> lista = new JList<String>();
+		lista.setBounds(0, 0, 1, 1);
+		getContentPane().add(lista);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 }

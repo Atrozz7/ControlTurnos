829aa7998e2d65d08988aaf42a25316bd76b4342
Aperkat <alvarosagamer@gmail.com>
2026-01-28 09:43:40 +0100

    Cliente pantalla

--- src/es/studium/ControlTurnos/ClientePantalla.java
+++ src/es/studium/ControlTurnos/ClientePantalla.java
@@ -1,5 +1,31 @@
 package es.studium.ControlTurnos;
+/**
+ *@autor Alvaro Martos
+  **/
+import javax.swing.*;
 
-public class ClientePantalla {
-
+public class ClientePantalla extends JFrame {
+    
+    public ClientePantalla() {
+        initialize();
+    }
+    
+    private void initialize() {
+        setTitle("Control de Turnos");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 800, 600);
+        
+        JPanel contentPane = new JPanel();
+        setContentPane(contentPane);
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() { 
+            	ClientePantalla frame = new ClientePantalla();
+                frame.setVisible(true);
+            }
+        });
+    }
 }

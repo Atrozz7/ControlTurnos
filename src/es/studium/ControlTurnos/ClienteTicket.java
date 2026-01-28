7c70bbe1ceb4f0a4de1a95c2ff328c0016bea51c
PDJ-gtb <pacodejesus99@gmail.com>
2026-01-28 09:01:25 +0100

    commit Ticket Paco

--- src/es/studium/ControlTurnos/ClienteTicket.java
+++ src/es/studium/ControlTurnos/ClienteTicket.java
@@ -2,4 +2,45 @@ package es.studium.ControlTurnos;
 
 public class ClienteTicket {
 
+	String numero;
+	String fecha;
+	String hora;
+	
+	public ClienteTicket() {
+		numero="";
+		fecha="";
+		hora="";
+	}
+	
+	public ClienteTicket(String numero, String fecha,String hora) {
+		this.numero=numero;
+		this.fecha=fecha;
+		this.hora=hora;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+	
+	
 }

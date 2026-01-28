03ce0ac9fc371c5df2bdb896cd399e2671e14107
Aperkat <alvarosagamer@gmail.com>
2026-01-28 10:33:18 +0100

    kPotasio

--- src/es/studium/ControlTurnos/ClientePantalla.java
+++ src/es/studium/ControlTurnos/ClientePantalla.java
@@ -1,15 +1,16 @@
 package es.studium.ControlTurnos;
+
 import javax.swing.*;
 
 public class ClientePantalla extends JFrame {
     
-    /**
-	 * @autor Alvaro Martos
-	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+    private JTextArea textArea;
+    private JScrollPane scrollPane;
 
-	public ClientePantalla() {
+    public ClientePantalla() {
         initialize();
+        mostrarTickets();
     }
     
     private void initialize() {
@@ -18,14 +19,30 @@ public class ClientePantalla extends JFrame {
         setBounds(100, 100, 800, 600);
         
         JPanel contentPane = new JPanel();
+        contentPane.setLayout(null);
         setContentPane(contentPane);
+        
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+        
+        scrollPane = new JScrollPane(textArea);
+        scrollPane.setBounds(20, 20, 740, 530);
+        contentPane.add(scrollPane);
+    }
+    
+    private void mostrarTickets() {
+        textArea.setText("");
+        textArea.append("Ticket 001 - Fecha: 2024-01-01 - Hora: 10:00\n");
+        textArea.append("Ticket 002 - Fecha: 2024-01-01 - Hora: 10:15\n");
+        textArea.append("Ticket 003 - Fecha: 2024-01-01 - Hora: 10:30\n");
+        textArea.append("Ticket 004 - Fecha: 2024-01-01 - Hora: 10:45\n");
     }
     
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() { 
-            	ClientePantalla frame = new ClientePantalla();
+                ClientePantalla frame = new ClientePantalla();
                 frame.setVisible(true);
             }
         });

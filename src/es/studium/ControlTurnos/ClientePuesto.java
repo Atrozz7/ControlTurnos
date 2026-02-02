bee1fd86d01e974e6d8f93a6c08da6116406fd51
dariapabonbotija <daria.pabon.botija@gmail.com>
2026-02-02 09:15:35 +0100

    Conexión y funcionamiento del botón ClientePuesto

--- src/es/studium/ControlTurnos/ClientePuesto.java
+++ src/es/studium/ControlTurnos/ClientePuesto.java
@@ -3,6 +3,10 @@ package es.studium.ControlTurnos;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 
@@ -16,6 +20,9 @@ public class ClientePuesto extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JTextField textField;
+	
+	String ipServidor = "localhost";
+    int puerto = 6666;
 
 	public ClientePuesto() {
 		setTitle("Cliente Puesto");
@@ -24,24 +31,56 @@ public class ClientePuesto extends JFrame {
 		getContentPane().setLayout(null);
 
 		JButton btnNewButton = new JButton("Libre");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-		
-			}
-		});
-		btnNewButton.setBounds(118, 148, 186, 79);
-		getContentPane().add(btnNewButton);
+        btnNewButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // LLAMADA AL MÉTODO QUE CONECTA CON EL SERVIDOR
+                puestoLibre();
+            }
+        });
+        btnNewButton.setBounds(118, 148, 186, 79);
+        getContentPane().add(btnNewButton);
 
-		textField = new JTextField();
-		textField.setEnabled(false);
-		textField.setBounds(133, 59, 159, 54);
-		getContentPane().add(textField);
-		textField.setColumns(10);
+        textField = new JTextField();
+        textField.setEditable(false);
+        textField.setBounds(133, 59, 159, 54);
+        getContentPane().add(textField);
+        textField.setColumns(10);
 
-		setVisible(true);
-	}
+        setVisible(true);
+    }
+    
+    private void puestoLibre() {
+        try {
+       
+            Socket socket = new Socket(ipServidor, puerto);
+            DataOutputStream salida = new DataOutputStream(socket.getOutputStream());
+            DataInputStream entrada = new DataInputStream(socket.getInputStream());
 
-	public static void main(String[] args) {
-		new ClientePuesto();
-	}
+          
+            salida.writeUTF("SIGUIENTE_PUESTO");
+
+          
+            String respuesta = entrada.readUTF();
+
+            if (respuesta.equals("COLA_VACIA")) {
+                textField.setText("Sin turnos");
+            } else {
+               
+                textField.setText(respuesta);
+            }
+
+
+            salida.close();
+            entrada.close();
+            socket.close();
+
+        } catch (IOException ex) {
+            textField.setText("Error Conexión");
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new ClientePuesto();
+    }
 }
